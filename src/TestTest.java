ced66d783b7f4ba2463f7c6fe7e0457ba0eba20c
Roussel <kleppi@gmx.de>
2014-06-04 09:29:03 +0200

    das ist nur ein Test

---
+++ src/TestTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Russi
+ * Date: 04.06.14
+ * Time: 08:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo MIA Klasse!");
+    }
+}
