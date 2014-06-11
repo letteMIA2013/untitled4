7507174a787fc7087920dfebf9b1ea3f6f00e600
e3_richter <huhaua@hotmail.de>
2014-06-11 08:33:42 +0200

    hi

--- src/TestTest.java
+++ src/TestTest.java
@@ -9,7 +9,7 @@ public class TestTest {
 
     public static void main(String[] args) {
         System.out.println("Hallo Du da!");
-        System.out.println("ich bins, Marc!");
+        System.out.println("ich bins, Marius!");
         System.out.println("hallo!");
         System.out.println("hallo!");
     }
